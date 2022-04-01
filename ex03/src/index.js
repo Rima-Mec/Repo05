d8af0768b16aae7aec44cc9cf9eb391b2b32ea7b
Rima-Mec <dmerima08@gmail.com>
2022-04-01 23:09:04 +0200

    Adding index.js file

---
+++ ex03/src/index.js
@@ -0,0 +1,10 @@
+function main() { 
+    // only change code below this line
+    var myLink = "<a href=\"htpp://example.com\" target=\"_blank\">Link</a>"
+    myLink = '<a href="htpp://example.com" target="blank">Link</a>'
+  // only change code above this line
+ return myLink;
+
+}
+ console.log(main());
+ module.exports = main;
