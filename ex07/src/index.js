4c3a6cd2cc0d4c742f81aa0203033b55a1bae48c
Rima-Mec <dmerima08@gmail.com>
2022-04-02 03:35:01 +0200

    Adding index.js file

---
+++ ex07/src/index.js
@@ -0,0 +1,11 @@
+function main() { 
+    // only change code below this line
+    var myName = "Merima";
+    var myString = "My name is "+ myName +" and I am well!";
+    // myString is now "My name is Merima and I am well!";
+    // only change code above this line
+ return myString;
+
+}
+ console.log(main());
+ module.exports = main;
