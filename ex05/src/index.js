7dbdb9575f916b5f26f404dcd7d9e2d03e3c75a0
Rima-Mec <dmerima08@gmail.com>
2022-04-02 02:54:50 +0200

    Adding file index.js

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,11 +1,11 @@
 function main() { 
     // only change code below this line
-    var myString = "This is the First String." "This is the Second String.";
-    myString = "This is the First String" + "This is the Second String";
-    // myString is "This is the First String. This is the Second String"
+    var myString = "This is the First String. This is the Second String.";
+    var myString = "This is the First String. "+"This is the Second String.";
+    // myString is "This is the First String. This is the Second String";
     // only change code above this line
  return myString;
-}
 
+}
  console.log(main());
  module.exports = main;
