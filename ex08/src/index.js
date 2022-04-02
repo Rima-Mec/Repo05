0c5c26d80a959e939bc1999b925fb4b6247ba248
Rima-Mec <dmerima08@gmail.com>
2022-04-02 03:49:20 +0200

    Adding index.js file

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main() { 
+    // only change code below this line
+    var myAdjective = "the best!";
+    var myString = "Mistral is ";
+    myString += myAdjective;
+    // myString is now "Mistral is the best!"
+    // only change code above this line
+ return myString;
+
+}
+ console.log(main());
+ module.exports = main;
