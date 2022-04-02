957f1309911c33b497ca96608bebb4c5622d70cb
Rima-Mec <dmerima08@gmail.com>
2022-04-02 03:54:58 +0200

    Adding index.js file

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -3,7 +3,7 @@ function main() {
     var myString = "Concatenation is the process of appednding one string to the end of another string.";
     var myString = "Conncatenation is the proccess off appending ";
     myString +="one string to the end ofanother string."
-    // myString is now "Concatenation is the procces of appending one string to the end of another string";
+    // myString is now "Concatenation is the procces of appending one string to the end of another string"
     // only change code above this line
  return myString;
 
